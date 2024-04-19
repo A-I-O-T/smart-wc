462d9ed4dd5c1c62b8fae6653055ca6fe97ae263
秋风 <15088601208@163.com>
2024-04-19 18:17:33 +0800

    feat: 快速启动脚步
    检查 git 和 node 环境

--- quickstart.sh
+++ quickstart.sh
@@ -2,17 +2,17 @@
 
 echo "Checking git..."
 if ! [ -x "$(command -v git)" ]; then
-    echo 'Error: git is not installed.' >&2
+    echo 'Error: git is not installed.'
     exit 1
 fi
 
 echo "Checking node..."
 if ! [ -x "$(command -v node)" ]; then
-    echo 'Error: Node.js is not installed.' >&2
+    echo 'Error: Node.js is not installed.'
     exit 1
 fi
 if [ "$(node -v | cut -d. -f1 | sed 's/[^0-9]*//g')" -lt 18 ]; then
-    echo 'Error: Node.js version >= 18.0.0' >&2
+    echo 'Error: Node.js version >= 18.0.0'
     exit 1
 fi
 
