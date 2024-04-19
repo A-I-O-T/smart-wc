424197da14f8dff40b22c7ee9a71fe756d33f2d9
秋风 <15088601208@163.com>
2024-04-19 18:13:07 +0800

    feat: 快速启动脚步
    检查 git 和 node 环境

--- quickstart.sh
+++ quickstart.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# 检查 是否安装了 git
+if ! [ -x "$(command -v git)" ]; then
+    echo 'Error: git is not installed.' >&2
+    exit 1
+fi
+# 检查是否按照了 node 且node 版本 >= 18
+if ! [ -x "$(command -v node)" ]; then
+    echo 'Error: Node.js is not installed.' >&2
+    exit 1
+fi
+if [ "$(node -v | cut -d. -f1 | sed 's/[^0-9]*//g')" -lt 18 ]; then
+    echo 'Error: Node.js version >= 18.0.0' >&2
+    exit 1
+fi
+
 git clone https://github.com/A-I-O-T/smart-wc.git
 
 cd smart-wc
